95fb4fd1f92c11611d55c45745dc3eb9b6e8f725
dsotgiu <domesotgiu@gmail.com>
2025-10-11 10:12:09 +0200

    feat: added formatting method

--- processor/src/main/java/domenico/sotgiu/processor/classmapping/ClassFieldNameMapper.java
+++ processor/src/main/java/domenico/sotgiu/processor/classmapping/ClassFieldNameMapper.java
@@ -7,6 +7,7 @@ import domenico.sotgiu.processor.util.ReplaceOf;
 
 import javax.lang.model.element.ElementKind;
 import java.util.Optional;
+import java.util.function.Function;
 
 
 public interface ClassFieldNameMapper extends FieldNameMapper {
@@ -17,7 +18,9 @@ public interface ClassFieldNameMapper extends FieldNameMapper {
             var repl = ReplaceOf.of("this");
             var defReplied = repl.apply(annotation.map(FileColumn::format).orElse("this"),
                     FileColumnDefault.from(annotation).get());
-            return repl.apply(defReplied,
+            Function<String, String> methodRepl = string -> ReplaceOf.of("method")
+                    .apply(string, "e");
+            return repl.andThen(methodRepl).apply(defReplied,
                     e.getKind() == ElementKind.METHOD ?
                             "e." + fieldName + "()" :
                             "e.get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1) + "()");
--- processor/src/main/java/domenico/sotgiu/processor/recordmapping/RecordFieldNameMapper.java
+++ processor/src/main/java/domenico/sotgiu/processor/recordmapping/RecordFieldNameMapper.java
@@ -6,16 +6,20 @@ import domenico.sotgiu.processor.util.FileColumnDefault;
 import domenico.sotgiu.processor.util.ReplaceOf;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 public interface RecordFieldNameMapper extends FieldNameMapper {
     static FieldNameMapper of(){
-      return e->{
-          var annotation = Optional.ofNullable(e.getAnnotation(FileColumn.class));
+      return element->{
+          var annotation = Optional.ofNullable(element.getAnnotation(FileColumn.class));
 
           var repl = ReplaceOf.of("this");
+
+          Function<String, String> methodRepl = string -> ReplaceOf.of("method")
+                  .apply(string, "e");
           var defReplied = repl.apply(annotation.map(FileColumn::format).orElse("this"),
                   FileColumnDefault.from(annotation).get());
-          return repl.apply(defReplied, "e."+e.getSimpleName().toString()+"()");
+          return repl.andThen(methodRepl).apply(defReplied, "e."+element.getSimpleName().toString()+"()");
       };
     }
 }
